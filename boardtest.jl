97f0bf7703dcd6578ffa3a8ac2315856badd8f03
Chris Foster <chris42f@gmail.com>
2020-02-15 21:29:28 +1000

    boardtest.jl comments + using fix + lower temp

--- boardtest.jl
+++ boardtest.jl
@@ -1,10 +1,15 @@
+# This file tests out ways of generating "maze-like" boards by randomly walking
+# agents.
+
 using StaticArrays
 using LinearAlgebra
+using Distributions
 
 include("types.jl")
 include("display.jl")
 include("maze_levels.jl")
 
+# Generate a unique path
 function unique_path(gen)
     i = 1
     while true
@@ -26,7 +31,7 @@ function make_candidates(generator)
     while true
         board = fill(' ', (width,height))
         coeffs = generator()
-        board = run_walkers(board, (d, env)->perceptron(coeffs, 1.0, d, env))
+        board = run_walkers(board, (d, env)->perceptron(coeffs, 0.2, d, env))
         clear_screen(stdout)
         printboard(stdout, board)
         println(stdout)
@@ -59,6 +64,16 @@ end
 #perceptrons_new = make_candidates(()->randn(3,25) .^3)
 #append!(perceptrons, perceptrons_new)
 #@save fname perceptrons
+
+# Some of the generation schemes resulted in weird distribution normalizations
+# One way to normalize them...
+#=
+for i=1:length(perceptrons)
+    rating,coeffs = perceptrons[i]
+    perceptrons[i] = rating=>coeffs ./ std(coeffs)
+end
+@save "perceptrons_1.jld2" perceptrons
+=#
 
 #=
 sheight,swidth = displaysize(stdout)
