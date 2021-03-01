5c03974d29e2e7971435b989be746ac969393bfd
Chris Foster <chris42f@gmail.com>
2021-03-01 19:57:51 +1000

    ECS: Explosions, animated sprites, sprite lifetime

--- ecs.jl
+++ ecs.jl
@@ -8,6 +8,11 @@ const VI = SA{Int}
 #-------------------------------------------------------------------------------
 # Components
 
+@component struct TimerComp
+    time::Int
+end
+TimerComp() = TimerComp(0)
+
 @component struct SpatialComp
     position::Vec2I
     velocity::Vec2I
@@ -24,9 +29,18 @@ end
     draw_priority::Int
 end
 
+@component struct AnimatedSpriteComp
+    icons::Vector{Char}
+end
+
+@component struct ExplosionComp
+    deadline::Int
+    radius::Int
+end
+
 @component struct PlayerComp
     number::Int
-    # items::Items
+    items::Dict{Char,Int}
 end
 
 @component struct PlayerLeftControlComp
@@ -38,10 +52,12 @@ end
 @component struct RandomVelocityControlComp
 end
 
-@component struct TimerComp
-    time::Int
+@component struct EntityKillerComp
 end
 
+@component struct LifetimeComp
+    max_age::Int
+end
 
 #-------------------------------------------------------------------------------
 # Systems
@@ -55,10 +71,9 @@ Overseer.requested_components(::TimerUpdate) = (TimerComp,)
 function Overseer.update(::TimerUpdate, m::AbstractLedger)
 	timer = m[TimerComp]
     for e in @entities_in(timer)
-        timer[e] += 1
+        timer[e] = TimerComp(timer[e].time + 1)
 	end
 end
-
 
 # Position update
 
@@ -100,6 +115,82 @@ function Overseer.update(::RandomVelocityUpdate, m::AbstractLedger)
 	end
 end
 
+# Explosions
+
+struct TimedExplosion <: System end
+
+Overseer.requested_components(::TimedExplosion) = (SpatialComp,TimerComp,ExplosionComp,EntityKillerComp)
+
+function Overseer.update(::TimedExplosion, m::AbstractLedger)
+	spatial = m[SpatialComp]
+	timer = m[TimerComp]
+    explosion = m[ExplosionComp]
+    killer_comp = m[EntityKillerComp]
+    for e in @entities_in(spatial && timer && explosion)
+        t = timer[e].time
+        ex = explosion[e]
+        r = t - ex.deadline
+        if r == 0
+            killer_comp[e] = EntityKillerComp()
+        end
+        if r >= 0
+            position = spatial[e].position
+            for i=-r:r, j=-r:r
+                if abs(i) == r || abs(j) == r
+                    Entity(m, SpatialComp(position + VI[i,j], VI[0,0]),
+                           SpriteComp('💥', 50),
+                           TimerComp(),
+                           LifetimeComp(1),
+                           EntityKillerComp(),
+                          )
+                end
+            end
+            if r >= ex.radius
+                schedule_delete!(m, e)
+            end
+        end
+	end
+    delete_scheduled!(m)
+end
+
+# Sprites with finite lifetime
+struct LifetimeUpdate <: System end
+
+Overseer.requested_components(::LifetimeUpdate) = (TimerComp,LifetimeComp)
+
+function Overseer.update(::LifetimeUpdate, m::AbstractLedger)
+	timer = m[TimerComp]
+    lifetime = m[LifetimeComp]
+    for e in @entities_in(timer && lifetime)
+        if timer[e].time > lifetime[e].max_age
+            schedule_delete!(m, e)
+        end
+	end
+    delete_scheduled!(m)
+end
+
+
+# Spatially deleting entities
+struct EntityKillUpdate <: System end
+
+Overseer.requested_components(::EntityKillUpdate) = (SpatialComp,EntityKillerComp)
+
+function Overseer.update(::EntityKillUpdate, m::AbstractLedger)
+	spatial = m[SpatialComp]
+    killer_tag = m[EntityKillerComp]
+    killer_positions = Set{Vec2I}()
+    for e in @entities_in(spatial && killer_tag)
+        push!(killer_positions, spatial[e].position)
+    end
+    for e in @entities_in(spatial)
+        if !(e in killer_tag) && (spatial[e].position in killer_positions)
+            schedule_delete!(m, e)
+        end
+	end
+    delete_scheduled!(m)
+end
+
+
 # Player Control
 
 struct PlayerRightControlUpdate <: System end
@@ -115,19 +206,53 @@ function Overseer.update(::PlayerRightControlUpdate, m::AbstractLedger)
                key == ARROW_LEFT  ? VI[-1,0] :
                key == ARROW_RIGHT ? VI[1, 0] :
                VI[0,0]
-    do_balloon = key == ' '
+    use_item = key == ' '
     for e in @entities_in(spatial_comp && control_comp)
         s = spatial_comp[e]
         spatial_comp[e] = SpatialComp(s.position, velocity)
-        if do_balloon
+        if use_item
+            # Some tests of various entity combinations
+
+            # Rising Balloon
             Entity(m, SpatialComp(s.position, VI[0,1]),
-                   SpriteComp('🎈', 1))
+                   SpriteComp('💠', 1))
+
+            # Ticking, random walking bomb. Lol
+            clocks = collect("🕛🕐🕑🕒🕓🕔🕕🕖🕗💣🕘💣🕙💣🕚")
+            Entity(m, SpatialComp(s.position, VI[0,0]),
+                   # RandomVelocityControlComp(),
+                   TimerComp(),
+                   SpriteComp('💣', 1),
+                   AnimatedSpriteComp(clocks),
+                   ExplosionComp(length(clocks), 1))
+
+            # Fruit random walkers :-D
+            Entity(m, SpatialComp(s.position, VI[0,0]),
+                   RandomVelocityControlComp(),
+                   SpriteComp(rand(collect("🍅🍆🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓")), 1))
         end
 	end
 end
 
 
-# Rendering
+# Graphics & Rendering
+
+struct AnimatedSpriteUpdate <: System end
+
+Overseer.requested_components(::AnimatedSpriteUpdate) = (TimerComp,SpriteComp,AnimatedSpriteComp)
+
+function Overseer.update(::AnimatedSpriteUpdate, m::AbstractLedger)
+	sprite = m[SpriteComp]
+	anim_sprite = m[AnimatedSpriteComp]
+    timer = m[TimerComp]
+    for e in @entities_in(sprite && timer && anim_sprite)
+        t = timer[e].time
+        sprites = anim_sprite[e].icons
+        sprite[e] = SpriteComp(sprites[mod1(t,length(sprites))],
+                               sprite[e].draw_priority)
+	end
+end
+
 
 struct TerminalRenderer <: System end
 
@@ -142,9 +267,8 @@ function Overseer.update(::TerminalRenderer, m::AbstractLedger)
     board = fill(' ', m.board_size...)
     for obj in drawables
         pos = obj.spatial.position
-        icon = obj.sprite.icon
         if 1 <= pos[1] <= m.board_size[1] && 1 <= pos[2] <= m.board_size[2]
-            board[pos...] = icon
+            board[pos...] = obj.sprite.icon
         end
     end
     printboard(m, board)
@@ -166,7 +290,9 @@ function Gameoji(term)
     ledger = Ledger(
         Stage(:control, [RandomVelocityUpdate(), PlayerRightControlUpdate()]),
         Stage(:dynamics, [PositionUpdate()]),
-        Stage(:rendering, [TerminalRenderer()])
+        Stage(:lifetime, [LifetimeUpdate(), TimedExplosion(), EntityKillUpdate()]),
+        Stage(:rendering, [AnimatedSpriteUpdate(), TerminalRenderer()]),
+        Stage(:dynamics_post, [TimerUpdate()]),
     )
     Gameoji(term, '\0', board_size, ledger)
 end
@@ -198,25 +324,30 @@ include("terminal.jl")
 function main()
     term = TerminalMenus.terminal
     game = init_game(term)
-    rawmode(term) do
-        in_stream = term.in_stream
-        clear_screen(stdout)
-        while true
-            update(game)
-            #=
-            if bytesavailable(in_stream) == 0
-                # Avoid repeated input lag by only drawing when no
-                # bytes are available.
-                draw(board, sprites)
-            end
-            =#
-            key = read_key()
-            if key == CTRL_C
-                # Clear
+    open("log.txt", "w") do logio
+        with_logger(ConsoleLogger(logio)) do
+            rawmode(term) do
+                in_stream = term.in_stream
                 clear_screen(stdout)
-                return
+                while true
+                    update(game)
+                    flush(logio) # Hack!
+                    #=
+                    if bytesavailable(in_stream) == 0
+                        # Avoid repeated input lag by only drawing when no
+                        # bytes are available.
+                        draw(board, sprites)
+                    end
+                    =#
+                    key = read_key()
+                    if key == CTRL_C
+                        # Clear
+                        clear_screen(stdout)
+                        return
+                    end
+                    game.input_key = key
+                end
             end
-            game.input_key = key
         end
     end
 end
